2e611048190b3c8c1c73d62e8d8d49f37118d722
DESKTOP-MA85DGD\DELL <huynhtienphat1811@gmail.com>
2024-11-06 16:55:10 +0700

    fix bill admin

--- src/AdminComponents/Bill/BillTable.jsx
+++ src/AdminComponents/Bill/BillTable.jsx
@@ -18,6 +18,8 @@ import {
   InputAdornment,
   MenuItem,
   Select,
+  FormControl,
+  InputLabel,
 } from "@mui/material";
 import CreateIcon from "@mui/icons-material/Create";
 import SearchIcon from "@mui/icons-material/Search";
@@ -52,23 +54,34 @@ const BillTable = () => {
   const { bills } = useSelector((state) => state.billReducer);
   const [currentPage, setCurrentPage] = useState(1);
   const jwt = localStorage.getItem("jwt");
-
+  const [stateFilter, setStateFilter] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
 
   const [deleteId, setDeleteId] = useState(null);
   const handlePageChange = (event, value) => {
     const params = new URLSearchParams(location.search);
     const accountId = params.get("accountId");
+    const state = params.get("state");
+    const phone = params.get("phone");
+    params.set("page", value);
+    if (state) {
+      params.set("state", state);
+    }
+    if (phone) {
+      params.set("phone", phone);
+    }
+    navigate(`${location.pathname}?${params.toString()}`, { replace: true });
 
     if (accountId) {
       params.set("accountId", accountId);
     }
     setCurrentPage(value);
-    console.log("page", value);
     dispatch(
       getBills({
         page: value,
         accountId,
+        phone: phone,
+        state: state,
       })
     );
   };
@@ -78,14 +91,29 @@ const BillTable = () => {
 
     const params = new URLSearchParams(location.search);
     const accountId = params.get("accountId");
+    const state = params.get("state");
     console.log("check param", accountId);
+
     if (searchTerm) {
       params.set("phone", searchTerm);
+    } else {
+      params.delete("phone");
+    }
+    if (state) {
+      params.set("state", state);
     }
 
-    dispatch(getBills({ page: 1, accountId, searchTerm }));
-  };
+    navigate(`${location.pathname}?${params.toString()}`, { replace: true });
 
+    dispatch(
+      getBills({
+        page: 1,
+        accountId,
+        phone: searchTerm,
+        state: state,
+      })
+    );
+  };
   const [searchParams] = useSearchParams();
 
   const params = new URLSearchParams(location.search);
@@ -97,32 +125,99 @@ const BillTable = () => {
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
-    const accountId = params.get("accountId");
-    console.log("check param", accountId);
-    dispatch(getBills({ page: 1, accountId }));
-  }, [dispatch]);
-  const handleStatusChange = (id, newStatus) => {
 
-    dispatch(updateBillStatus(id, newStatus)); 
-    const params = new URLSearchParams(location.search);
-    const accountId = params.get("accountId");
-  
-    dispatch(getBills({ page: currentPage, accountId }));
+    const page = params.get("page") || 1;
+    const phone = params.get("phone") || "";
+    const state = params.get("state") || "";
+    const accountId = params.get("accountId") || ""; 
+    console.log("check params:", { page, phone, state, accountId });
 
+    dispatch(
+      getBills({
+        page,
+        accountId,
+        phone,
+        state,
+      })
+    );
+  }, [dispatch, location.search]);
+  const handleStatusChange = async (id, newStatus) => {
+    try {
+      await dispatch(updateBillStatus(id, newStatus));
+
+      const params = new URLSearchParams(location.search);
+      const accountId = params.get("accountId");
+
+      const page = params.get("page") || 1;
+      const phone = params.get("phone") || "";
+      const state = params.get("state") || "";
+      setStateFilter(state);
+      navigate(`${location.pathname}?${params.toString()}`, { replace: true });
+
+      dispatch(
+        getBills({
+          page: page,
+          accountId: accountId,
+          phone: phone,
+          state: state,
+        })
+      );
+    } catch (error) {
+      console.error("Error updating bill status:", error);
+    }
   };
 
-  const handleClearSearch = () => setSearchTerm("");
+  const handleClearSearch = () => {
+    const params = new URLSearchParams(location.search);
+
+    params.delete("page");
+    params.delete("accountId");
+    params.delete("state");
+    params.delete("phone");
+
+    navigate(`${location.pathname}?${params.toString()}`, { replace: true });
+    dispatch(
+      getBills({
+        page: 1,
+      })
+    );
+
+    setSearchTerm("");
+    setStateFilter("");
+  };
+
+  const handleStateChange = (event) => {
+    const params = new URLSearchParams(location.search);
+
+    params.set("state", event.target.value);
+
+    const page = params.get("page") || 1;
+    const phone = params.get("phone") || "";
+    const accountId = params.get("accountId") || "";
+    const state = params.get("state") || "";
+    setStateFilter(event.target.value);
+    navigate(`${location.pathname}?${params.toString()}`, { replace: true });
+
+    dispatch(
+      getBills({
+        page: page,
+        accountId: accountId,
+        phone: phone,
+        state: event.target.value,
+      })
+    );
+  };
 
   return (
     <Box sx={{ width: "95%", margin: "0px auto", marginTop: "100px" }}>
       <Box
         sx={{
           display: "flex",
-          justifyContent: "space-between",
           alignItems: "center",
           marginBottom: "20px",
         }}
       >
+
         <TextField
           label="Search"
           variant="outlined"
@@ -133,20 +228,33 @@ const BillTable = () => {
               borderRadius: "20px",
               height: "40px",
             },
+            marginRight: 2, 
           }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={handleSearch}>
-                  <SearchIcon />
-                </IconButton>
-                <IconButton onClick={handleClearSearch}>
-                  <ClearIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
+         
         />
+
+        <FormControl variant="outlined" sx={{ width: 200 }}>
+          <InputLabel>Trạng thái</InputLabel>
+          <Select
+            value={stateFilter}
+            onChange={handleStateChange}
+            label="Trạng thái"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <MenuItem value={1}>Đang xử lý</MenuItem>
+            <MenuItem value={2}>Đang thực hiện món</MenuItem>
+            <MenuItem value={3}>Đang giao hàng</MenuItem>
+            <MenuItem value={4}>Hoàn tất</MenuItem>
+          </Select>
+        </FormControl>
+        <InputAdornment position="end">
+          <IconButton onClick={handleSearch}>
+            <SearchIcon />
+          </IconButton>
+          <IconButton onClick={() => handleClearSearch()} edge="end">
+            <ClearIcon />
+          </IconButton>
+        </InputAdornment>
       </Box>
 
       {/* Table Section */}
@@ -188,16 +296,24 @@ const BillTable = () => {
                     <TableCell align="center">
                       <TextField
                         select
-                        value={item.state} 
-                        onChange={(e) => handleStatusChange(item._id, e.target.value)}
+                        value={item.state}
+                        onChange={(e) =>
+                          handleStatusChange(item._id, e.target.value)
+                        }
                         variant="outlined"
                         size="small"
                         onClick={(e) => e.stopPropagation()}
-                        InputProps={{ readOnly: false }} 
+                        InputProps={{ readOnly: false }}
                       >
-                        <MenuItem value={1}>Đang xử lí</MenuItem>
-                        <MenuItem value={2}>Đang thực hiện món</MenuItem>
-                        <MenuItem value={3}>Đang giao hàng</MenuItem>
+                        <MenuItem value={1} disabled={item.state >= 2}>
+                          Đang xử lí
+                        </MenuItem>
+                        <MenuItem value={2} disabled={item.state >= 3}>
+                          Đang thực hiện món
+                        </MenuItem>
+                        <MenuItem value={3} disabled={item.state === 4}>
+                          Đang giao hàng
+                        </MenuItem>
                         <MenuItem value={4}>Hoàn tất</MenuItem>
                       </TextField>
                     </TableCell>
